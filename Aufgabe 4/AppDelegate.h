9d04099fcb40aecca44d555117f466ec9cc5601d
Krebs2012 <anton_beuthhochschule@hotmail.de>
2013-11-27 11:33:49 +0100

    Initial Commit

---
+++ Aufgabe 4/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Aufgabe 4
+//
+//  Created by Master-Student on 27.11.13.
+//  Copyright (c) 2013 Master-Student. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Aufgabe 4/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Aufgabe 4
+//
+//  Created by Master-Student on 27.11.13.
+//  Copyright (c) 2013 Master-Student. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
